ef1ac69d875f2d96211eac6d1a02477a9c8c2ad2
Cristina Gonzalez <cristina.glez.rubio@gmail.com>
2019-08-05 09:49:00 +0200

    inital commit

---
+++ 2.11/js/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+const input = document.querySelector('.app__input');
+const button = document.querySelector('.app__button');
+const list = document.querySelector('.app__list');
+
+
+function wookiee () {
+    cosnt ENDPOINT  = 'https://swapi.co/api/people';
+    const res = '';
+
+    
+
+}
+
+button.addEventListener('click', wookiee);
