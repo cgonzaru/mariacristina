29f2245d2699d460a5b1961dbc715227474ec473
Cristina Gonzalez <cristina.glez.rubio@gmail.com>
2019-08-05 10:00:23 +0200

    ej1

--- 2.12/ej1/js/main.js
+++ 2.12/ej1/js/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const list = document.querySelector('.list');
+
+const arr = [1,2,3];
+
+for (let i=0; i<arr.length; i++) {
+    const newItem = document.createElement('li');
+    const newLiContent = document.createTextNode(arr[i]);
+
+    newItem.appendChild(newLiContent);
+    list.appendChild(newItem);
+
+}
