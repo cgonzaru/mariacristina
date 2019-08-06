443d667d74d56400027444172a3dbcfbc1331d3a
mariamata <matah.maria@gmail.com>
2019-08-06 08:53:55 +0200

    ejercicio 2 ok

--- 2.12/ej2/js/main.js
+++ 2.12/ej2/js/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const select = document.querySelector('.select');
+const opt1 = document.querySelector('.opt1');
+const opt2 = document.querySelector('.opt2');
+const opt3 = document.querySelector('.opt3');
+
+const img = document.querySelector('.img')
+console.log(select.value);
+function changeImage(){
+
+    if(select.value === opt1.value){
+        img.src = 'https//:placehold.it/200x200';
+        img.appendChild(img.src);
+        
+    } else {
+        console.log('no funciona');
+    }
+};
+
+select.addEventListener('change', changeImage);
+
--- 2.13/ej2/js/main.js
+++ 2.13/ej2/js/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+let counter = 0;
+let timer;
+let counterUva = '';
+
+const uva = document.querySelector('.uva');
+
+const incrementAndShowCounter = () => {
+  counter++;
+  counterUva += '🥭' ;
+  const time = document.querySelector('.time');
+  time.innerHTML = counter;
+  uva.innerHTML = counterUva;
+
+  if(counter === 12){
+      clearInterval(timer);
+  }
+};
+
+timer = setInterval(incrementAndShowCounter, 1000);
